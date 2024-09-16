80b4c07fe7ff6b758e3e9ffd8382f92967da6b73
Ash digit <ashdigit44@gmail.com>
2024-09-16 10:32:06 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,44 +1,27 @@
+const cors = require("cors");
 const express = require("express");
-const errroHandler = require("./error-handler-middleware")
-const {
-  getTopics,
-  getApi
-  } = require("./controllers/topics.controller")
+const errroHandler = require("./error-handler-middleware");
+const { getTopics, getApi } = require("./controllers/topics.controller");
 const {
   getArticleById,
   getArticles,
   getCommentsByArticleId,
   updateVoteArticle,
-  deleteComment
-} = require("./controllers/articles.controller")
+  deleteComment,
+} = require("./controllers/articles.controller");
 
 const app = express();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get("/api/topics", getTopics)
-app.get("/api", getApi)
-app.get("/api/articles/:article_id", getArticleById)
-app.get("/api/articles", getArticles)
-app.get("/api/articles/:article_id/comments",getCommentsByArticleId)
-app.patch("/api/articles/:article_id", updateVoteArticle)
-app.delete("/api/comments/:comment_id", deleteComment)
+app.get("/api/topics", getTopics);
+app.get("/api", getApi);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.patch("/api/articles/:article_id", updateVoteArticle);
+app.delete("/api/comments/:comment_id", deleteComment);
 
-app.use(errroHandler)
-
-
-// app.use((err, req, res, next) => {
-//   if (err.status === 404) {
-//       res.status(err.status).send({status: 404, msg: err.msg });
-//   }
-//   if(err.status === 400){
-//     res.status(err.status).send({status: 400, msg: err.msg });
-//   }
-//   if(err.status === undefined){
-//     res.status(400).send({status: 400, msg: "Bad Request", info: "PSQL/QUERY/NOT-VALID"})
-//   }
-//   if(err.status === 406){
-//     res.status(406).send(err)
-//   }
-// });
+app.use(errroHandler);
 
 module.exports = app;
